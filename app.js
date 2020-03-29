5c420fb72b3961380a79390cf117f12a86cab41a
Tito Yak <titokuol@college.harvard.edu>
2020-03-30 02:08:44 +0300

    set get all tours route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api/books', (req, res) => {
     res.status(200).json({ message: 'Hello from the server side', app: "booksreview"});
 });
 
