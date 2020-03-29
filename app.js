8466abf16e5ed27518f52113df1dc2670645296e
Tito Yak <titokuol@college.harvard.edu>
2020-03-30 02:13:35 +0300

    use middleware express.json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 
+// Use middlewares
+app.use(express.json());
+
 app.get('/api/books', (req, res) => {
     res.status(200).json({ message: 'Hello from the server side', app: "booksreview"});
 });
