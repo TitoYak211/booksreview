49f17a51ff76b5179802216bcc70409e9480ffaa
Tito Yak <titokuol@college.harvard.edu>
2020-03-31 01:15:39 +0300

    routes - add param middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,10 @@ const userController = require('./../controllers/userController');
 // Routes
 const router = express.Router();
 
+router.param('id', (req, res, next, value) => {
+    next();
+});
+
 router.route('/').get(userController.getAllUsers).post(userController.addUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
