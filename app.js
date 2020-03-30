831852dabd601af5ebd4e83817c8308825ffa010
Tito Yak <titokuol@college.harvard.edu>
2020-03-30 22:59:40 +0300

    add user routes

--- app.js
+++ app.js
@@ -35,7 +35,33 @@ const deleteBook = (req, res) => {
     res.status(204).json({ message: 'Delete a book given id', app: "booksreview"});
 };
 
+const getAllUsers = (req, res) => {
+    res.status(200).json({ message: 'These are all users', app: "booksreview" });
+};
+
+const getUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(200).json({ message: 'Get a user by id', app: "booksreview"});
+};
+
+const addUser = (req, res) => {
+    res.status(201).json({ message: 'New user added', app: "booksreview"});
+};
+
+const updateUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(200).json({ message: 'Update a user given an id', app: "booksreview"});
+};
+
+const deleteUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(204).json({ message: 'Delete a user given an id', app: "booksreview"});
+};
+
 app.route('/api/books').get(getAllBooks).post(addBook);
 app.route('/api/books/:id').get(getBook).patch(updateBook).delete(deleteBook);
+
+app.route('/api/users').get(getAllUsers).post(addUser);
+app.route('/api/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = app;
