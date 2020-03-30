1e394dad0cfd31e9708a6eab2cc472dd688a7b2c
Tito Yak <titokuol@college.harvard.edu>
2020-03-30 03:14:23 +0300

    get a book by id

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/api/books', (req, res) => {
     res.status(200).json({ message: 'These are all books', app: "booksreview"});
 });
 
+app.get('/api/books/:id', (req, res) => {
+    console.log(req.params);
+    res.status(200).json({ message: 'Get a book by id', app: "booksreview"});
+});
+
 app.post('/api/books', (req, res) => {
     res.status(200).json({ message: 'New book added', app: "booksreview"});
 });
