fb4f62e8d081dcd84cb44a2e09c69334c79437bc
Tito Yak <titokuol@college.harvard.edu>
2020-03-31 00:52:09 +0300

    controllers - factor out user handlers

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+// Routes handlers
+exports.getAllUsers = (req, res) => {
+    res.status(200).json({ message: 'These are all users', app: "booksreview" });
+};
+
+exports.getUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(200).json({ message: 'Get a user by id', app: "booksreview"});
+};
+
+exports.addUser = (req, res) => {
+    res.status(201).json({ message: 'New user added', app: "booksreview"});
+};
+
+exports.updateUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(200).json({ message: 'Update a user given an id', app: "booksreview"});
+};
+
+exports.deleteUser = (req, res) => {
+    const id = req.params.id * 1;
+    res.status(204).json({ message: 'Delete a user given an id', app: "booksreview"});
+};
