39a5e0139345ba7a8f33647f653a006879f0e11a
Tito Yak <titokuol@college.harvard.edu>
2020-04-02 02:36:42 +0300

    routes | utilities - factor out books features
    - this modulates BooksFeatures for reuse

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,68 +1,7 @@
 const Book = require('./../models/bookModel');
+const BooksFeatures = require('./../utilities/features');
 
-// Routes handlers
-class BooksFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    };
-
-    filter() {
-        // Buid a filter query
-        const queryObj = { ...this.queryString };
-        const removedFields = ['page', 'sort', 'limit', 'fields'];
-        removedFields.forEach(el => delete queryObj[el]);
-
-        // Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-        this.query = this.query.find(JSON.parse(queryStr));
-
-        return this;
-    };
-
-    sort() {
-        // Sorting books based on query object
-        if (this.queryString.sort) {
-            const sortBy = this.queryString.sort.split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-        } else {
-            this.query = this.query.sort('-year');
-        };
-
-        return this;
-    };
-
-    displayFields() {
-        // Fields limiting
-        if (this.queryString.fields) {
-            const fields = this.queryString.fields.split(',').join(' ');
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select('-__v');
-        };
-
-        return this;
-    };
-
-    paginate() {
-        // PAGINATION: Default page is 1
-        const page = this.queryString.page * 1 || 1;
-
-        // Default number of books per page = 100
-        const limit = this.queryString.limit * 1 || 100;
-
-        // Number of books to skip for the requested page
-        const skipValue = (page - 1) * limit;
-
-        this.query = this.query.skip(skipValue).limit(limit);
-
-        return this;
-    };
-};
-
+// ROUTES HANDLERS
 exports.popularBooks = async (req, res, next) => {
     try {
         req.query.limit = 10;
