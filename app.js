6da1ea5b4028bceb66347237ce3b726d45d12867
Tito Yak <titokuol@college.harvard.edu>
2020-04-03 00:45:57 +0300

    routes - create global errorhandling middleware

--- app.js
+++ app.js
@@ -29,4 +29,17 @@ app.all('*', (req, res, next) => {
     next();
 });
 
+// Global ErrorHandling middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).JSON({
+        status: err.status,
+        message: err.message
+    });
+
+    next();
+});
+
 module.exports = app;
