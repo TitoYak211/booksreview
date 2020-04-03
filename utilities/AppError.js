d1ae08e0f4473c39f905587605df762287d472b8
Tito Yak <titokuol@college.harvard.edu>
2020-04-03 03:38:03 +0300

    utilities - define application error class

---
+++ utilities/AppError.js
@@ -0,0 +1,15 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+        this.isOperational = true;
+
+        // Eliminate this class from error stack
+        Error.captureStackTrace(this, this.constructor);
+    };
+};
+
+module.exports = AppError;
