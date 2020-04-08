11c3f4fbd9cad50f527dbae18c6fafa6a1136297
Tito Yak <titokuol@college.harvard.edu>
2020-04-08 16:33:51 +0300

    nodemailer - define mail sending functions

--- utilities/nodemail.js
+++ utilities/nodemail.js
@@ -1,5 +1,13 @@
 const nodemailer = require('nodemailer');
 
-const sendMail = options => {
-    // TODO
-}
+const sendMail = async options => {
+    // Mail sending service e.g gmail
+    const mailer = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.USER_EMAIL,
+            pass: process.env.USER_PASSWORD
+        }
+    });
+
