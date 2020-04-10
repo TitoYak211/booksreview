71d113adfc8bb434d5340b6cb9be2c7f3fac4f4c
Tito Yak <titokuol@college.harvard.edu>
2020-04-10 20:01:29 +0300

    reviewModel - show virtual properties

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-    review: {
-        type: String,
-        required: [true, 'Review cannot be empty']
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review cannot be empty']
+        },
+        rating: {
+            type: String,
+            min: 1,
+            max: 5
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        book: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Book',
+            required: [true, 'A review must belong to a book.']
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'A review must belong to a user.']
+        }
     },
-    rating: {
-        type: String,
-        min: 1,
-        max: 5
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
     }
-});
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 
