67f951b817eef697dd977253de47343a9944e57d
Tito Yak <titokuol@college.harvard.edu>
2020-04-11 00:09:22 +0300

    bookRoute | reviewController - implement nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,6 +14,15 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+    // Allow nested routes
+    if (!req.body.book) {
+        req.body.book = req.params.bookId;
+    };
+
+    if (!req.body.user) {
+        req.body.user = req.user.id;
+    };
+
     const newReview = await Review.create(req.body);
 
     res.status(201).JSON({
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bookController = require('./../controllers/bookController');
 const paramMiddlewares = require('./../controllers/paramMiddlewares');
 const authentication = require('./../controllers/authentication');
+const reviewController = require('./../controllers/reviewController');
 
 // Routes
 const router = express.Router();
@@ -21,5 +22,8 @@ router.route('/:id')
     .get(paramMiddlewares.checkID, bookController.getBook)
     .patch(authentication.protectRoutes, paramMiddlewares.checkID, bookController.updateBook)
     .delete(authentication.protectRoutes, authentication.restrictRole('admin'), paramMiddlewares.checkID, bookController.deleteBook);
+
+router.route('/:bookId/reviews')
+    .post(authentication.protectRoutes, reviewController.createReview);
 
 module.exports = router;
