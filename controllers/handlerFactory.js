cac224be9fb9ef513c4ed20927281d9b00807e85
Tito Yak <titokuol@college.harvard.edu>
2020-04-11 03:35:58 +0300

    handlerFactory - delete any record

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,17 @@
+const catchAsync = require('./../utilities/catchAsync');
+const AppError = require('./../utilities/AppError');
+
+exports.deleteDoc = Model => {
+    catchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndDelete(req.params.id);
+    
+        if (!doc) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+    
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    });
+};
