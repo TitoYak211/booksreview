20a504dc2e189352904a2fa859f15a987cf0938a
Tito Yak <titokuol@college.harvard.edu>
2020-04-27 23:46:27 +0300

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -62,7 +62,6 @@ const sendErrorProd = (err, req, res) => {
   };
 
   if (err.isOperational) {
-    console.log(err);
     return res.status(err.statusCode).render('error', {
       title: 'Something went wrong!',
       message: err.message
